0c46e4f4d0db41e660deb99580b53e5125548f0a
uzillion <uzair_inamdar@hotmail.com>
2018-06-13 01:38:18 -0700

    Add gdax trade watch

--- bin/message.js
+++ bin/message.js
@@ -9,12 +9,12 @@ const build = (messageObj) => {
 
   let encoded_message = "";
   if(exchange.toLowerCase() == "binance")
-    encoded_message = encodeURIComponent(`${event}:\nBTC/USDT (${exchange})\n${quantity} at $${price}`);
+    encoded_message = encodeURIComponent(`${event}:\n${symbol} (${exchange})\n${quantity} at $${price}`);
   else
     if(quantity < 0)
-      encoded_message = encodeURIComponent(`${event}:\nBTC/USD (${exchange})\nSold ${quantity*-1} at $${price}`);
+      encoded_message = encodeURIComponent(`${event}:\n${symbol} (${exchange})\nSold ${quantity*-1} at $${price}`);
     else
-      encoded_message = encodeURIComponent(`${event}:\nBTC/USD (${exchange})\nBought ${quantity} at $${price}`);
+      encoded_message = encodeURIComponent(`${event}:\n${symbol} (${exchange})\nBought ${quantity} at $${price}`);
   
   var chatOptions = {
     uri: `https://api.telegram.org/bot596257066:AAGkmCVBSgYx0FvPV-OElc7ZwTypnLj4ipw/sendMessage?chat_id=-1001236925237&text=${encoded_message}`,
--- bin/trade.js
+++ bin/trade.js
@@ -1,13 +1,17 @@
 const request = require('request-promise-native');
 const message = require('./message');
 
+let prev_maker_order_id = "";
+let prev_taker_order_id = "";
+let prev_quantity = 0;
+
 const binance = (data) => {
   let trade = JSON.parse(data);
   let quantity = parseFloat(trade.q);
   let price = parseFloat(trade.p);
   let symbol = trade.s;
   
-  if((symbol == "BTCUSDT" && quantity > 4) || (symbol == "EOSBTC" && quantity > 1000)) {
+  if((symbol == "BTCUSDT" && quantity > 7) || (symbol == "EOSBTC" && quantity > 1000)) {
   var tickerOptions = {
     uri: `https://api.binance.com/api/v1/ticker/24hr?symbol=${symbol}`,
     headers: {
@@ -47,7 +51,7 @@ const bitfinex = (data) => {
     channel_id = trade[0];
 
   if(trade[2] != undefined)
-  if(trade[1] == "tu" && Math.abs(trade[2][2]) > 4) {
+  if(trade[1] == "tu" && Math.abs(trade[2][2]) > 7) {
     let quantity = trade[2][2];
     let price = trade[2][3]
     var tickerOptions = {
@@ -79,6 +83,50 @@ const bitfinex = (data) => {
 }
 
 const gdax = (data) => {
-
+  let trade = JSON.parse(data);
+  let maker_order_id = trade.maker_order_id;
+  let taker_order_id = trade.taker_order_id;
+  let quantity = parseFloat(trade.size);
+  let price = trade.price;
+  let side = trade.side;
+  if((maker_order_id === prev_maker_order_id) || (taker_order_id === prev_taker_order_id)) {
+    quantity = quantity + prev_quantity;
+    prev_quantity = quantity;
+  } else if(typeof trade.size !== "undefined") {
+    prev_maker_order_id = maker_order_id;
+    prev_taker_order_id = taker_order_id;
+    prev_quantity = quantity;
+  }
+  if(quantity > 7) {
+    var tickerOptions = {
+      uri: `https://api.gdax.com/products/BTC-USD/ticker`,
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true
+    };
+  
+    request(tickerOptions)
+    .then(function (tickerResponse) {
+      // console.log(tickerResponse);
+      let volume = parseFloat(tickerResponse.volume);
+      console.log(0.001*volume)
+      if(quantity > (0.001*volume)) {
+        let messageObj = {
+          event: "TRADE",
+          symbol: "BTC/USD",
+          quantity,
+          price,
+          exchange: "gdax"
+        }
+        if(side == "sell")
+          messageObj.quantity *= -1;
+        message(messageObj);
+      }
+      
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
 }
 module.exports = {binance, bitfinex, gdax};

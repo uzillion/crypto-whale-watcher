48a80b5fd56ac033538c54d3495ba3bab8dee834
Uzair Inamdar <uzair_inamdar@hotmail.com>
2020-05-17 05:38:23 -0700

    Updated documentation and customizability of exchages
    Apologies as documentation did not mention anything about database migrations.
    
    Also added a check to see if database is connected. If connections fails, suggestions will be outputted.
    
    You can now disable exchanges if exchanges if you don't want data from them. This is still a work in progress, and the implementation isn't the cleanest but it works.
    
    Ideally I would want to ignore if an exchange is unable to get data for a certain cryptocurrency. Any help would be appreciated.

--- db/index.js
+++ db/index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
 const pg = require("pg-promise")();
-const db = pg(process.env.DATABASE_URL);
+let db;
+try {
+    db = pg(process.env.DATABASE_URL);
+    console.log('\033[92m\u2713\033[0m DB connected');
+} catch {
+    console.log('\033[91m\u2717\033[0m Could not connect to database.');
+    console.log('Please make sure you have run \'npm run db:migrate\' first');
+    console.log('Also, make sure correct environment variables are set.');
+    process.exit(1);
+}
+
 module.exports = db;
